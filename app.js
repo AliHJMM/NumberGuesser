3d2ebbe6f1e28c8e5719413e75c5b2b899cacc47
Ali <AliHasanJMM@gmail.com>
2024-07-27 16:28:40 +0300

    added displayMsg function

--- app.js
+++ app.js
@@ -3,3 +3,7 @@
 let mysteryNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
+
+const displayMsg = function (msg) {
+    document.querySelector('.status-msg').textContent = msg;
+  };
