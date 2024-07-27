adf641e2d00f690f0fb637ec2a0105899a4e8ac7
Ali <AliHasanJMM@gmail.com>
2024-07-27 17:34:45 +0300

    ui.js comments

--- src/js/ui.js
+++ src/js/ui.js
@@ -1,3 +1,5 @@
+// Update the displayed message
 export const displayMsg = function (msg) {
+  // Select the element with the class 'status-msg' and set its text content to the provided message
   document.querySelector('.status-msg').textContent = msg;
 };
