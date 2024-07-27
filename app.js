59121d2b6d78e15f950a8b8592a28598ae9d8e9b
Ali <AliHasanJMM@gmail.com>
2024-07-27 16:33:08 +0300

    Submit Btn Event Listener

--- app.js
+++ app.js
@@ -7,3 +7,40 @@ let highScore = 0;
 const displayMsg = function (msg) {
     document.querySelector('.status-msg').textContent = msg;
   };
+
+  document.querySelector('.submit').addEventListener('click', function () {
+    const prediction = Number(document.querySelector('.user-prediction').value);
+    console.log(prediction, typeof prediction);
+  
+  
+    if (!prediction) {
+    
+      displayMsg('⛔️ No Number!');
+  
+    } else if (prediction === mysteryNumber) {
+     
+        displayMsg('🎉 Correct Number!');
+      document.querySelector('.hidden-number').textContent = mysteryNumber;
+  
+      document.querySelector('body').style.backgroundColor = '#60b347';
+      document.querySelector('.hidden-number').style.width = '30rem';
+  
+      if (score > highScore) {
+        highScore = score;
+        document.querySelector('.best-score').textContent = highScore;
+      }
+  
+    } else if (prediction !== mysteryNumber) {
+      if (score > 1) {
+       
+        displayMsg(prediction > mysteryNumber ? '📈 Too high!' : '📉 Too low!');
+        score--;
+        document.querySelector('.score').textContent = score;
+      } else {
+      
+        displayMsg('💥 You lost the game!');
+        document.querySelector('.score').textContent = 0;
+      }
+    }
+  
+  });
